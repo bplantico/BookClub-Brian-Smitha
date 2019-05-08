5355fb43154dd09ee3e10f2c60781edeef868171
Smitha Hosmani <32909448+hsmitha26@users.noreply.github.com>
2019-05-08 17:14:11 -0600

    Adds new book sans authors and default image All tests passing
    Co-authored-by: Brian Plantico <43261385+bplantico@users.noreply.github.com>

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,6 +10,22 @@ class BooksController < ApplicationController
   end
 
   def new
+    @book = Book.new
+  end
 
+  def create
+    book = Book.new(book_params)
+    book.save
+    #before saving, condition for book exits and titlize
+    #before saving, condition for author exists and comma-separated stuff
+    #conditional for default image, after saving
+    #after saving, authors, if new will be added to db.
+    redirect_to book_path(book)
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :pages, :year_pub, :cover_img)
   end
 end
--- spec/features/books/new_spec.rb
+++ spec/features/books/new_spec.rb
@@ -13,6 +13,24 @@ RSpec.describe "As a visitor, " do
 
       click_link "Add New Book"
       expect(current_path).to eq(new_book_path)
+
+      fill_in :book_title, with: "Title 1"
+      fill_in :book_pages, with: 304
+      fill_in :book_year_pub, with: 1901
+      fill_in :book_cover_img, with: "http://clipart-library.com/images/BigKKBybT.png"
+      fill_in :book_authors, with: "Author 1, Author 2"
+
+      click_button "Add Book"
+
+      new_book = Book.last
+
+      expect(current_path).to eq(book_path(new_book))
+
+      expect(page).to have_content(new_book.title)
+      expect(page).to have_content(new_book.pages)
+      expect(page).to have_content(new_book.year_pub)
+      expect(page).to have_css("img[src='#{new_book.cover_img}']")
+      # expect(page).to have_content("Author 1")
     end
   end
 end
