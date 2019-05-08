5612ad3b7c0aa888a493204e70c3ca6a1f98be67
Brian Plantico <bplantico@gmail.com>
2019-05-07 18:31:38 -0600

    Adds book show page with title, pages, year_pub, and image.
    Co-Author-By: Smitha Hosmani <32909448+hsmitha26@users.noreply.github.com>

---
+++ spec/features/books/show_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+RSpec.describe "as a visitor, " do
+  describe "when I visit a book show page" do
+    before :each do
+      @book_1 = Book.create!(title: "Title 1", pages: 100, year_pub: 1901, cover_img: "https://media.wired.com/photos/5be4cd03db23f3775e466767/master/pass/books-521812297.jpg")
+      @book_2 = Book.create!(title: "Title 2", pages: 200, year_pub: 1902, cover_img: "https://media.wired.com/photos/5be4cd03db23f3775e466767/master/pass/books-521812297.jpg")
+      @book_3 = Book.create!(title: "Title 3", pages: 300, year_pub: 1903, cover_img: "https://media.wired.com/photos/5be4cd03db23f3775e466767/master/pass/books-521812297.jpg")
+
+      @author_1 = @book_1.authors.create!(name: "Author 1", author_img: "https://banner2.kisspng.com/20180516/zce/kisspng-shadow-person-dungeons-dragons-silhouette-art-5afc1fa5cf7d87.5508397315264726138499.jpg")
+      @author_2 = @book_1.authors.create!(name: "Author 2", author_img: "https://banner2.kisspng.com/20180516/zce/kisspng-shadow-person-dungeons-dragons-silhouette-art-5afc1fa5cf7d87.5508397315264726138499.jpg")
+      @author_3 = @book_2.authors.create!(name: "Author 3", author_img: "https://banner2.kisspng.com/20180516/zce/kisspng-shadow-person-dungeons-dragons-silhouette-art-5afc1fa5cf7d87.5508397315264726138499.jpg")
+      @author_4 = @book_3.authors.create!(name: "Author 4", author_img: "https://banner2.kisspng.com/20180516/zce/kisspng-shadow-person-dungeons-dragons-silhouette-art-5afc1fa5cf7d87.5508397315264726138499.jpg")
+    end
+
+    it "shows details about the books" do
+
+      visit books_path
+
+      within "#test-book-index-#{@book_1.id}" do
+        click_link("#{@book_1.title}")
+      end
+
+      expect(current_path).to eq("/books/#{@book_1.id}")
+      expect(page).to have_content(@book_1.title)
+      expect(page).to have_content(@book_1.pages)
+      expect(page).to have_content(@book_1.year_pub)
+      expect(page).to have_css("img[src='#{@book_1.cover_img}']")
+
+    end
+  end
+end
