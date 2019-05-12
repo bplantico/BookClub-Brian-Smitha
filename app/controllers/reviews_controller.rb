20186013a0cae2733912951487b418fcc0e61c48
Brian Plantico <bplantico@gmail.com>
2019-05-12 11:26:41 -0600

    IN PROGRESS working on adding a new review

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,5 +2,20 @@
 
 class ReviewsController < ApplicationController
 
+  def new
+    @review = Review.new
+  end
+
+  def create
+    # username = params[:review][:username]
+    # User.name.find do |name|
+
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:title, :rating, :body)
+  end
 
 end
--- spec/features/reviews/new_spec.rb
+++ spec/features/reviews/new_spec.rb
@@ -4,30 +4,37 @@ require 'rails_helper'
 
 RSpec.describe "As a visitor, " do
   describe "when I visit a books show page, " do
-    before :each do
-      @book_1 = Book.create!(title: "Title 1", pages: 100, year_pub: 1901, cover_img: "https://media.wired.com/photos/5be4cd03db23f3775e466767/master/pass/books-521812297.jpg")
-      @book_2 = Book.create!(title: "Title 2", pages: 200, year_pub: 1902, cover_img: "https://media.wired.com/photos/5be4cd03db23f3775e466767/master/pass/books-521812297.jpg")
-      @user_1 = User.create!(name: "User One")
-      @user_2 = User.create!(name: "User Two")
-    end
+    # before :each do
+    #   @book_1 = Book.create!(title: "Title 1", pages: 100, year_pub: 1901, cover_img: "https://media.wired.com/photos/5be4cd03db23f3775e466767/master/pass/books-521812297.jpg")
+    #   @book_2 = Book.create!(title: "Title 2", pages: 200, year_pub: 1902, cover_img: "https://media.wired.com/photos/5be4cd03db23f3775e466767/master/pass/books-521812297.jpg")
+    #   @user_1 = User.create!(name: "User One")
+    #   @user_2 = User.create!(name: "User Two")
+    # end
 
     it "I see a link to add a new review" do
-      visit book_path(@book_1)
+      book_1 = Book.create!(title: "Title 1", pages: 100, year_pub: 1901, cover_img: "https://media.wired.com/photos/5be4cd03db23f3775e466767/master/pass/books-521812297.jpg")
+      visit book_path(book_1)
 
       expect(page).to have_link("Add A Review")
     end
 
     it "I am taken to a form to add new book after I click on Add A Review" do
-      visit book_path(@book_1)
+      book_1 = Book.create!(title: "Title 1", pages: 100, year_pub: 1901, cover_img: "https://media.wired.com/photos/5be4cd03db23f3775e466767/master/pass/books-521812297.jpg")
+      user_1 = User.create!(name: "User One")
+      brian = User.create!(name: "brian plantico")
+
+      visit book_path(book_1)
 
       click_link "Add A Review"
+
       expect(current_path).to eq(new_review_path)
 
       fill_in :review_title, with: "reView oNe  "
+      fill_in :review_name, with: "Brian Plantico"
       fill_in :review_rating, with: 5
       fill_in :review_body, with: "This is by far the absolute best Book I have ever read."
-
       click_button "Create Review"
+
 
       new_review = Review.last
 
@@ -36,6 +43,7 @@ RSpec.describe "As a visitor, " do
       expect(page).to have_content("Title One")
 
       expect(page).to have_content(new_review.title)
+
       expect(page).to have_content(new_review.rating)
       expect(page).to have_content(new_review.body)
 
