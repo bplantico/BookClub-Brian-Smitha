32eb9466b2046816366382b89a9eba5056729163
Brian Plantico <bplantico@gmail.com>
2019-05-14 11:24:05 -0600

    Adds statistics to book show pages: - Average rating for book - Three best reviews - Three worst reviews - Three best and worst review sections don't appear if no reviews

--- spec/features/books/show_spec.rb
+++ spec/features/books/show_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe "as a visitor, " do
   describe "when I visit a book show page" do
     before :each do
 
-      @book_1 = Book.create!(title: "Title 1", pages: 100, year_pub: 1901, cover_img: "https://media.wired.com/photos/5be4cd03db23f3775e466767/master/pass/books-521812297.jpg")
+      @book_1 = Book.create!(title: "Test Title 1", pages: 100, year_pub: 1901, cover_img: "https://media.wired.com/photos/5be4cd03db23f3775e466767/master/pass/books-521812297.jpg")
       @book_2 = Book.create!(title: "Title 2", pages: 200, year_pub: 1902, cover_img: "https://media.wired.com/photos/5be4cd03db23f3775e466767/master/pass/books-521812297.jpg")
       @book_3 = Book.create!(title: "Title 3", pages: 300, year_pub: 1903, cover_img: "https://media.wired.com/photos/5be4cd03db23f3775e466767/master/pass/books-521812297.jpg")
 
@@ -19,12 +19,16 @@ RSpec.describe "as a visitor, " do
 
       @user_1 = User.create!(name: "User One")
       @user_2 = User.create!(name: "User Two")
+      @user_3 = User.create!(name: "User Three")
+      @user_4 = User.create!(name: "User Four")
 
       @review_1 = @book_1.reviews.create!(title: "Review 1", rating: 1, body: "stuff 1", user: @user_1 )
       @review_2 = @book_1.reviews.create!(title: "Review 2", rating: 2, body: "stuff 2", user: @user_2 )
+      @review_4 = @book_1.reviews.create!(title: "Review 4", rating: 4, body: "stuff 4", user: @user_3 )
+      @review_5 = @book_1.reviews.create!(title: "Review 5", rating: 5, body: "stuff 5", user: @user_4 )
 
       @review_3 = @book_2.reviews.create!(title: "Review 3", rating: 1, body: "stuff 3", user: @user_1 )
-      
+
     end
 
     it "shows details about the books" do
@@ -69,7 +73,7 @@ RSpec.describe "as a visitor, " do
     it "when I click on the Delete Book link, I am taken to Books Index page where I no longer see the deleted book." do
       visit book_path(@book_1)
       expect(page).to have_link("Delete Book")
-      expect(Review.count).to eq(3)
+      expect(Review.count).to eq(5)
       expect(Book.count).to eq(3)
 
       click_link "Delete Book"
@@ -81,6 +85,50 @@ RSpec.describe "as a visitor, " do
       expect(page).to have_content(@author_2.name)
       expect(Review.count).to eq(1)
       expect(Book.count).to eq(2)
+    end
+
+    it "I see an area on the page for statistics about reviews" do
+      visit book_path(@book_1)
+      
+      within "#book-show-page-stats" do
+        expect(page).to have_content("Average Rating: #{@book_1.average_rating}")
+
+        within "#best-three-reviews" do
+          expect(page).to have_content("Best Reviews:")
+          expect(page).to have_content("#{@review_5.title}")
+          expect(page).to have_content("#{@review_5.rating}")
+          expect(page).to have_content("#{@user_4.name}")
+
+          expect(page).to have_content("#{@review_4.title}")
+          expect(page).to have_content("#{@review_4.rating}")
+          expect(page).to have_content("#{@user_3.name}")
+
+          expect(page).to have_content("#{@review_2.title}")
+          expect(page).to have_content("#{@review_2.rating}")
+          expect(page).to have_content("#{@user_2.name}")
+        end
+
+        within "#worst-three-reviews" do
+          expect(page).to have_content("Worst Reviews:")
+          expect(page).to have_content("#{@review_1.title}")
+          expect(page).to have_content("#{@review_1.rating}")
+          expect(page).to have_content("#{@user_1.name}")
+
+          expect(page).to have_content("#{@review_2.title}")
+          expect(page).to have_content("#{@review_2.rating}")
+          expect(page).to have_content("#{@user_2.name}")
+
+          expect(page).to have_content("#{@review_4.title}")
+          expect(page).to have_content("#{@review_4.rating}")
+          expect(page).to have_content("#{@user_3.name}")
+        end
+
+
+      end
+      # - the top three reviews for this book (title, rating and user only)
+      # - the bottom three reviews for this book  (title, rating and user only)
+      # - the overall average rating of all reviews for this book
+
     end
   end
 end
