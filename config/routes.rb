8ef84e63a6f7354e3d7b8ec73a506fe18d6aa479
Brian Plantico <bplantico@gmail.com>
2019-06-20 11:20:26 -0600

    Rewrites routes so they're handrolled (previously routes used resources). All tests still passing

--- config/routes.rb
+++ config/routes.rb
@@ -3,16 +3,31 @@ Rails.application.routes.draw do
 
   root to: "welcome#index"
 
-  resources :books, only: [:index, :new, :create, :destroy]
+  get    '/books',       to: 'books#index', as: 'books'
+  post   '/books',       to: 'books#create'
+  get    '/books/new',   to: 'books#new',   as: 'new_book'
+  delete '/books/:id',   to: 'books#destroy'
 
-  resources :books, only: [:show] do
-    resources :reviews, only: [:new, :create]
-  end
+  # resources :books, only: [ :index, :new, :create, :destroy]
 
-  resources :authors, only: [:show, :destroy]
+  get  '/books/:id',                  to: 'books#show',     as: 'book'
+  get  '/books/:book_id/reviews/new', to: 'reviews#new',    as: "new_book_review"
+  post '/books/:book_id/reviews',     to: 'reviews#create', as: "book_reviews"
 
-  resources :users, only: [:show] do
-    resources :reviews, only: [:destroy]
-  end
+  # resources :books, only: [:show] do
+  #   resources :reviews, only: [:new, :create]
+  # end
+
+  get    '/authors/:id', to: 'authors#show', as: 'author'
+  delete '/authors/:id', to: 'authors#destroy'
+
+  # resources :authors, only: [:show, :destroy]
+
+  get    '/users/:id',                  to: 'users#show',      as: 'user'
+  delete '/users/:user_id/reviews/:id', to: 'reviews#destroy', as: 'user_review'
+
+  # resources :users, only: [:show] do
+  #   resources :reviews, only: [:destroy]
+  # end
 
 end
